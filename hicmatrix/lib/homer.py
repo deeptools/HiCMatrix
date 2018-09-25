e88900e8fdc61e16a2d6fcb1870f76293d1f2c34
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2018-09-25 15:20:23 +0200

    Adding write support for homer

--- hicmatrix/lib/homer.py
+++ hicmatrix/lib/homer.py
@@ -16,15 +16,16 @@ class Homer(MatrixFile, object):
 
         with open(self.matrixFileName, 'r') as matrix_file:
             values = matrix_file.readline()
-            values = values.split('\t')
+            values = values.strip().split('\t')
 
             # get bin size
             start_first = int(values[2].strip().split('-')[1])
             start_second = int(values[3].strip().split('-')[1])
             bin_size = start_second - start_first
+            log.debug('values {}'.format(values))
             for i, value in enumerate(values[2:]):
                 chrom, start = value.strip().split('-')
-                log.debug('chrom {} start {}'.format(chrom, start))
+                # log.debug('chrom {} start {}'.format(chrom, start))
 
                 cut_intervals.append((chrom, int(start), int(start) + bin_size, 1))
 
@@ -35,10 +36,26 @@ class Homer(MatrixFile, object):
                 for i, value in enumerate(values[2:]):
                     data.append(float(value))
                 matrix_dense.append(data)
-            log.debug('data {}'.format(data))
+            # log.debug('data {}'.format(data))
 
         matrix = csr_matrix(matrix_dense)
         nan_bins = None
         distance_counts = None
         correction_factors = None
         return matrix, cut_intervals, nan_bins, distance_counts, correction_factors
+
+    def save(self, pFilename,  pSymmetric=None, pApplyCorrection=None):
+        
+        with open(pFilename, 'w') as homerMatrixFile:
+            homerMatrixFile.write('HiCMatrix (directory=.)\tRegions\t')
+            for bin_interval in self.cut_intervals:
+                homerMatrixFile.write('{}-{}\t'.format(bin_interval[0], bin_interval[1]))
+            homerMatrixFile.write('\n')
+
+            for i in range(self.matrix.shape[0]):
+                data = '\t'.join(map(str, self.matrix[i, :].toarray().flatten()))
+                homerMatrixFile.write('{}-{}\t{}-{}\t'.format(self.cut_intervals[i][0], self.cut_intervals[i][1],
+                                                            self.cut_intervals[i][0], self.cut_intervals[i][1]))
+                homerMatrixFile.write('{}'.format(data))                                    
+                if i < self.matrix.shape[0] - 1:
+                    homerMatrixFile.write('\n')
